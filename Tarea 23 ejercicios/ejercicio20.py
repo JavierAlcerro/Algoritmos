1a66e8ef9fe7828c050df168ffc66da9f37be0da
JavierAlcerro <140477168+JavierAlcerro@users.noreply.github.com>
2024-12-09 22:46:48 -0600

    Add files via upload

---
+++ Tarea 23 ejercicios/ejercicio20.py
@@ -0,0 +1,65 @@
+class NodoArbol:
+    def __init__(self, valor):
+        self.valor = valor
+        self.izquierdo = None
+        self.derecho = None
+
+class ArbolBinario:
+    def __init__(self):
+        self.raiz = None
+
+    def insertar(self, valor):
+        if not self.raiz:
+            self.raiz = NodoArbol(valor)
+        else:
+            self._insertar_recursivo(self.raiz, valor)
+
+    def _insertar_recursivo(self, nodo, valor):
+        if valor < nodo.valor:
+            if nodo.izquierdo is None:
+                nodo.izquierdo = NodoArbol(valor)
+            else:
+                self._insertar_recursivo(nodo.izquierdo, valor)
+        else:
+            if nodo.derecho is None:
+                nodo.derecho = NodoArbol(valor)
+            else:
+                self._insertar_recursivo(nodo.derecho, valor)
+
+    def recorrido_inorden(self):
+        def _inorden(nodo):
+            if nodo:
+                _inorden(nodo.izquierdo)
+                print(nodo.valor, end=" ")
+                _inorden(nodo.derecho)
+
+        _inorden(self.raiz)
+        print()
+
+    def buscar(self, valor):
+        return self._buscar_recursivo(self.raiz, valor)
+
+    def _buscar_recursivo(self, nodo, valor):
+        if nodo is None:
+            return False
+        if nodo.valor == valor:
+            return True
+        elif valor < nodo.valor:
+            return self._buscar_recursivo(nodo.izquierdo, valor)
+        else:
+            return self._buscar_recursivo(nodo.derecho, valor)
+
+arbol = ArbolBinario()
+arbol.insertar(5)
+arbol.insertar(3)
+arbol.insertar(7)
+arbol.insertar(2)
+arbol.insertar(4)
+arbol.insertar(6)
+arbol.insertar(8)
+
+print("Recorrido inorden del arbol:")
+arbol.recorrido_inorden()
+
+print("Buscar 4 en el arbol:", arbol.buscar(4))
+print("Buscar 10 en el arbol:", arbol.buscar(10))
